2e990b4a9655040d811ed1a09c7a54efb8529223
Vineet Sepaha <Vineet Sepaha>
2016-09-27 22:13:49 -0700

    Selection with more agents

--- Assets/Scripts/AgentScript.cs
+++ Assets/Scripts/AgentScript.cs
@@ -7,21 +7,29 @@ public class AgentScript : MonoBehaviour
     private Vector3 targetPosition;
     public GameObject agent1;
     public GameObject agent2;
+    public GameObject agent3;
+    public GameObject agent4;
+    public GameObject agent5;
     private GameObject[] agents;
-    private NavMeshAgent[] agentMesh = new NavMeshAgent[2];
+    private NavMeshAgent[] agentMesh = new NavMeshAgent[5];
     private int selected;
 
     public Material selectionMaterial;
     public Material basicMaterial;
 
+    private int numberOfAgents;
+
     // Use this for initialization
     void Start()
     {
-        agents = new GameObject[2];
+        numberOfAgents = 5;
+        agents = new GameObject[numberOfAgents];
         agents[0] = agent1;
         agents[1] = agent2;
+        agents[2] = agent3;
+        agents[3] = agent4;
+        agents[4] = agent5;
         selected = -1;
-        int numberOfAgents = 2;
         for (int i = 0; i < numberOfAgents; i++)
         {
             agentMesh[i] = agents[i].GetComponent<NavMeshAgent>();
@@ -41,15 +49,51 @@ public class AgentScript : MonoBehaviour
                 if (hit.collider.tag == "Agent")
                 {
                     selected = 0;
-                    agents[0].GetComponent<Renderer>().material = selectionMaterial;
-                    agents[1].GetComponent<Renderer>().material = basicMaterial;
+                    for (int i = 0; i < numberOfAgents; i++)
+                    {
+                        agents[i].GetComponent<Renderer>().material = basicMaterial;
+                    }
+                    agents[selected].GetComponent<Renderer>().material = selectionMaterial;
                     return;
                 }
                 if (hit.collider.tag == "Agent1")
                 {
                     selected = 1;
-                    agents[1].GetComponent<Renderer>().material = selectionMaterial;
-                    agents[0].GetComponent<Renderer>().material = basicMaterial;
+                    for (int i = 0; i < numberOfAgents; i++)
+                    {
+                        agents[i].GetComponent<Renderer>().material = basicMaterial;
+                    }
+                    agents[selected].GetComponent<Renderer>().material = selectionMaterial;
+                    return;
+                }
+                if (hit.collider.tag == "Agent2")
+                {
+                    selected = 2;
+                    for (int i = 0; i < numberOfAgents; i++)
+                    {
+                        agents[i].GetComponent<Renderer>().material = basicMaterial;
+                    }
+                    agents[selected].GetComponent<Renderer>().material = selectionMaterial;
+                    return;
+                }
+                if (hit.collider.tag == "Agent3")
+                {
+                    selected = 3;
+                    for (int i = 0; i < numberOfAgents; i++)
+                    {
+                        agents[i].GetComponent<Renderer>().material = basicMaterial;
+                    }
+                    agents[selected].GetComponent<Renderer>().material = selectionMaterial;
+                    return;
+                }
+                if (hit.collider.tag == "Agent4")
+                {
+                    selected = 4;
+                    for(int i = 0; i < numberOfAgents; i++)
+                    {
+                        agents[i].GetComponent<Renderer>().material = basicMaterial;
+                    }
+                    agents[selected].GetComponent<Renderer>().material = selectionMaterial;
                     return;
                 }
                 targetPosition = hit.point;
