f41c75476cb23722e1c516a857192c9bfb897d45
Vineet Sepaha <Vineet Sepaha>
2016-10-11 22:34:47 -0700

    movement basics

--- Assets/Scripts/AvatarController.cs
+++ Assets/Scripts/AvatarController.cs
@@ -20,18 +20,25 @@ public class AvatarController : MonoBehaviour {
 	void Update () {
         animator.SetBool("isWalking", false);
         animator.SetBool("turnRight", false);
+        animator.SetBool("jump", false);
+        animator.SetBool("isWalkingBack", false);
 	    if (Input.GetKey(KeyCode.I))
         {
             animator.SetBool("isWalking", true);
-            transform.Translate(Vector3.forward * movingSpeed * Time.deltaTime);
+            //transform.Translate(Vector3.forward * movingSpeed * Time.deltaTime);
         }
         if (Input.GetKey(KeyCode.L))
         {
             animator.SetBool("turnRight", true);
         }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            animator.SetBool("jump", true);
+        }
         if (Input.GetKey(KeyCode.K))
         {
-            transform.Translate(Vector3.back * movingSpeed * Time.deltaTime);
+            //transform.Translate(Vector3.back * movingSpeed * Time.deltaTime);
+            animator.SetBool("isWalkingBack", true);
         }
 	}
 }
