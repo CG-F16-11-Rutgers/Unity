fe447342c6a8c260850a465c54c5df6c3fc7384d
Vineet Sepaha <Vineet Sepaha>
2016-10-11 21:43:09 -0700

    essence of animation

---
+++ Assets/Scripts/AvatarController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AvatarController : MonoBehaviour {
+
+    public int movingSpeed = 100;
+    private Animator animator;
+
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        animator.SetBool("isWalking", false);
+        animator.SetBool("turnRight", false);
+	    if (Input.GetKey(KeyCode.I))
+        {
+            animator.SetBool("isWalking", true);
+            transform.Translate(Vector3.forward * movingSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.L))
+        {
+            animator.SetBool("turnRight", true);
+        }
+        if (Input.GetKey(KeyCode.K))
+        {
+            transform.Translate(Vector3.back * movingSpeed * Time.deltaTime);
+        }
+	}
+}
