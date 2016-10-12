b78a881be22c5661b3830876434dffdef9880062
Vineet Sepaha <Vineet Sepaha>
2016-10-12 12:56:29 -0700

    major improvements

--- Assets/Scripts/AvatarController.cs
+++ Assets/Scripts/AvatarController.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class AvatarController : MonoBehaviour {
 
-    public int movingSpeed = 100;
+    //public int movingSpeed = 100;
     private Animator animator;
 
     void Awake()
@@ -13,13 +13,14 @@ public class AvatarController : MonoBehaviour {
 
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
         animator.SetBool("isWalking", false);
         animator.SetBool("turnRight", false);
         animator.SetBool("turnLeft", false);
         animator.SetBool("jump", false);
         animator.SetBool("isWalkingBack", false);
-	    if (Input.GetKey(KeyCode.W))
+        animator.SetBool("shiftPressed", false);
+        if (Input.GetKey(KeyCode.W))
         {
             animator.SetBool("isWalking", true);
             //transform.Translate(Vector3.forward * movingSpeed * Time.deltaTime);
@@ -41,6 +42,11 @@ public class AvatarController : MonoBehaviour {
         {
             animator.SetBool("jump", true);
         }
-      
-	}
+        if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+        {
+            animator.SetBool("shiftPressed", true);
+        }
+
+
+    }
 }
