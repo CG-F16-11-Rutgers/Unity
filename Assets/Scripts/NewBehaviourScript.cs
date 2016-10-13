307d619b3ecaa4d86093fc9ac2ebcca2033994ce
Vineet Sepaha <Vineet Sepaha>
2016-10-12 18:17:30 -0700

    enhanced movements

---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    private Animator animator;
+
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+	
+	// Update is called once per frame
+	void Update () {
+        animator.SetFloat("velx", 0.0f);
+        animator.SetBool("locomotion", false);
+        if (Input.GetKey(KeyCode.W))
+        {
+            animator.SetBool("locomotion", true);
+            animator.SetFloat("velx", 1.0f);
+            //transform.Translate(Vector3.forward * movingSpeed * Time.deltaTime);
+        }
+    }
+}
