b9945282ca1df85e9e9008ad5972f08a2b04d5dc
Michael <michaelshur28@gmail.com>
2016-10-13 17:25:53 -0400

    grab em by the pussy

---
+++ Assets/Scripts/SampleAgent.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SampleAgent : MonoBehaviour
+{
+    public float speed;
+
+    private Vector3 targetPosition;
+
+    private NavMeshAgent agent;
+    
+    private Animator animator;
+    
+
+    // Use this for initialization
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.speed = agent.speed * speed;
+        targetPosition = transform.position;
+
+        animator = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (agent.remainingDistance < 1.0F)
+        {
+
+            Debug.Log("I love big butts and I cannot lie");
+            agent.velocity = Vector3.zero;
+            agent.ResetPath();
+            animator.SetBool("isWalking", false);
+        }
+        else
+        {
+            Debug.Log("Grab em by the Pussy!");
+           // agent.SetDestination(targetPosition);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        { 
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+            {
+
+                animator.SetBool("isWalking", true);
+                targetPosition = hit.point;
+                agent.SetDestination(targetPosition);
+
+            }
+        }
+    }
+   
+}
