81445eaf90fe8ea946ab3cefd8b82e5c6ce534f4
Michael <michaelshur28@gmail.com>
2016-10-13 15:54:58 -0400

    kk

---
+++ Assets/Scripts/GuyControllerScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GuyControllerScript : MonoBehaviour {
+
+    private Animator animator;
+	
+	// Use this for initialization
+	void Start () {
+	
+        animator = GetComponent<Animator>();
+        
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        animator.SetBool("isWalking", false);
+        
+        if (Input.GetKey(KeyCode.W))
+        {
+            animator.SetBool("isWalking", true);
+            //transform.Translate(Vector3.forward * movingSpeed * Time.deltaTime);
+        }
+	}
+}
